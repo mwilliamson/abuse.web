2cd2151943b0621bb0e3183e24686329423d0e2f
Michael Williamson <mike@zwobble.org>
2010-12-10 23:25:21 +0000

    Adding a basic injector

---
+++ test/ZWOBBLE/abuse/web/inject_test.js
@@ -0,0 +1,58 @@
+var inject = require("ZWOBBLE/abuse/web/inject");
+
+exports.bindingInstanceToKeyResultsInThatInstanceAlwaysBeingReturned  = function(test) {
+    var injector = inject.injector(),
+        user = {name: "Bob"};
+    
+    injector.bind("user").toInstance(user);
+    
+    test.equal(user, injector.get("user"));
+    test.done();
+};
+
+exports.bindingInstanceToFunctionResultsInTheFunctionBeingCalledWhen = function(test) {
+    var injector = inject.injector(),
+        user = {name: "Bob"};
+    
+    injector.bind("user").toFunction(function() { return user; });
+    
+    test.equal(user, injector.get("user"));
+    test.done();
+};
+
+exports.injectorCanCallInjectableFunction = function(test) {
+    var injector = inject.injector(),
+        user = {name: "Bob"},
+        username = inject.injectable("user", function(user) {
+            return user.name;
+        });
+    
+    injector.bind("user").toFunction(function() { return user; });
+    
+    test.equal("Bob", injector.call(username));
+    test.done();
+};
+
+exports.callingNonInjectableFunctionCallsFunctionWithNoArgs = function(test) {
+    var injector = inject.injector(),
+        args,
+        username = function() {
+            args = arguments;
+            return "Bob";
+        };
+    
+    test.equal("Bob", injector.call(username));
+    test.equal(0, args.length);
+    test.done();
+};
+
+exports.canBindKeysToInjectableFunctions = function(test) {
+    var injector = inject.injector(),
+        user = {name: "Bob"};
+    
+    injector.bind("user").toInstance(user);
+    injector.bind("username").toFunction(inject.injectable("user", function(user) { return user.name; }));
+    
+    test.equal("Bob", injector.get("username"));
+    test.done();
+};
