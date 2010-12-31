00cf300de5ec0799cd1158ae5cb01adedb11ae64
Michael Williamson <mike@zwobble.org>
2010-12-31 17:01:43 +0000

    Clicking on warning selects relevant line in edit box

--- static/js/grammars/edit.js
+++ static/js/grammars/edit.js
@@ -1,12 +1,50 @@
 $(document).ready(function() {
     var timer,
         undefined,
+        setCursorPosition = function(elements, line, character) {
+            elements.each(function(index, element) {
+                // Need to calculate pos by reading out the textarea
+                var range,
+                    pos = 0,
+                    lines = $(element).val().split("\n"),
+                    i = 0;
+                if (line >= lines.length) {
+                    return;
+                }
+                while (i < line) {
+                    pos += lines[i].length + 1;
+                    i += 1;
+                }
+                //pos += character;
+                if (element.setSelectionRange) {
+                    element.setSelectionRange(pos, pos + lines[line].length);
+                } else if (element.createTextRange) {
+                    range = element.createTextRange();
+                    range.collapse(true);
+                    range.moveEnd('character', pos);
+                    range.moveStart('character', pos + lines[line].length);
+                    range.select();
+                }
+                $(element).focus();
+            });
+        },
+        createWarningLink = function(warning) {
+            var linkElement = $(document.createElement("span")).css({
+                    cursor: "pointer",
+                    color: "#003399",
+                    "text-decoration": "underline"
+                }).html(warning.str).click(function() {
+                    if (warning.lineNumber !== undefined) {
+                        setCursorPosition($("#grammarBody"), warning.lineNumber - 1, 0);
+                    }
+                }),
+                liElement = $(document.createElement("li")).html(linkElement);
+            return liElement;
+        };
         updateWarnings = function() {
             var body = $("#grammarBody").val(),
                 grammar = ZWOBBLE.abuse.parse(body),
-                warnings = $.map(grammar.errors, function(error) {
-                    return error.str;
-                }),
+                warnings = grammar.errors,
                 //allSentences = ZWOBBLE.abuse.generateAll(grammar.rules, 100),
                 warningsListElement = $("#warningsList"),
                 i;
@@ -14,7 +52,7 @@ $(document).ready(function() {
                 $("#warnings").show();
                 warningsListElement.html("");
                 for (i = 0; i < warnings.length; i += 1) {
-                    warningsListElement.append($(document.createElement("li")).html(warnings[i]))
+                    warningsListElement.append(createWarningLink(warnings[i]));
                 }
             } else {
                 $("#warnings").hide();
@@ -27,4 +65,5 @@ $(document).ready(function() {
         setTimeout(updateWarnings, 1000);
     });
     updateWarnings();
+    
 });
